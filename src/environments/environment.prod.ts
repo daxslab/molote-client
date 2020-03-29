5e78c8646025aae6d6e1fd88025363f9749c42ab
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2020-03-29 16:33:38 -0400

    fix prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-export const SERVER_URL = 'http://moloteapi.daxslab.com';
+export const SERVER_URL = 'https://moloteapi.daxslab.com';
 export const MAP_TILES = 'http:\\/\\/{s}.tiles.redcuba.cu\\/{z}\\/{x}\\/{y}.png';
